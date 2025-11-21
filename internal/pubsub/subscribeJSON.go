0b6eea3a4f3129590e9890f4e506ae880d94f329
JakubKyhos <kubakyhos4@gmail.com>
2025-11-21 14:33:53 +0100

    update main.go

---
+++ internal/pubsub/subscribeJSON.go
@@ -0,0 +1,14 @@
+package pubsub
+
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func SubscribeJSON[T any](
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	queueType SimpleQueueType, // an enum to represent "durable" or "transient"
+	handler func(T),
+) error
