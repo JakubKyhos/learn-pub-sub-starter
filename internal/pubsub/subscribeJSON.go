b02a6412512e7b58dcbef2e38bf4d78651e8b3ba
JakubKyhos <kubakyhos4@gmail.com>
2025-11-22 11:34:49 +0100

    add consumers

--- internal/pubsub/subscribeJSON.go
+++ internal/pubsub/subscribeJSON.go
@@ -1,6 +1,9 @@
 package pubsub
 
 import (
+	"encoding/json"
+	"fmt"
+
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -11,4 +14,35 @@ func SubscribeJSON[T any](
 	key string,
 	queueType SimpleQueueType, // an enum to represent "durable" or "transient"
 	handler func(T),
-) error
+) error {
+	ch, que, err := DeclareAndBind(conn, exchange, queueName, key, queueType)
+	if err != nil {
+		return fmt.Errorf("failed to declare and bind: %v", err)
+	}
+
+	msgs, err := ch.Consume(que.Name, "", false, false, false, false, nil)
+	if err != nil {
+		return fmt.Errorf("failed to start delivering messages: %v", err)
+	}
+
+	unmarshaller := func(data []byte) (T, error) {
+		var target T
+		err := json.Unmarshal(data, &target)
+		return target, err
+	}
+
+	go func() {
+		defer ch.Close()
+		for msg := range msgs {
+			target, err := unmarshaller(msg.Body)
+			if err != nil {
+				fmt.Printf("could not unmarshal message: %v\n", err)
+				continue
+			}
+			handler(target)
+			msg.Ack(false)
+		}
+	}()
+
+	return nil
+}
