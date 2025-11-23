1408030b647d72fd41a93135f0868a39501c53c5
JakubKyhos <kubakyhos4@gmail.com>
2025-11-23 11:23:15 +0100

    add gob encoding

---
+++ internal/pubsub/publishGob.go
@@ -0,0 +1,37 @@
+package pubsub
+
+import (
+	"bytes"
+	"context"
+	"encoding/gob"
+	"time"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func PublishGob[T any](ch *amqp.Channel, exchange, key string, val T) error {
+	var buffer bytes.Buffer
+	encoder := gob.NewEncoder(&buffer)
+	err := encoder.Encode(val)
+	if err != nil {
+		return err
+	}
+	return ch.PublishWithContext(context.Background(), exchange, key, false, false, amqp.Publishing{
+		ContentType: "application/gob",
+		Body:        buffer.Bytes(),
+	})
+}
+
+func PublishGameLog(publishCh *amqp.Channel, username, msg string) error {
+	return PublishGob(
+		publishCh,
+		routing.ExchangePerilTopic,
+		routing.GameLogSlug+"."+username,
+		routing.GameLog{
+			Username:    username,
+			CurrentTime: time.Now(),
+			Message:     msg,
+		},
+	)
+}
